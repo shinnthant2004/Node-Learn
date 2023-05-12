3a1f677016db5a7e2b4cfe3a941b1e7e5dbc8190
shinnthant2004 <shinthantequi@gmail.com>
2023-05-12 15:16:33 +0630

    parameterized URLS

--- server/index.js
+++ server/index.js
@@ -2,17 +2,39 @@ const http = require("http");
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 1,
+    name: "shinn thant",
+  },
+  {
+    id: 2,
+    name: "kaung thant",
+  },
+  {
+    id: 3,
+    name: "min thant",
+  },
+];
+
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
+  const route = req.url.split("/");
+  console.log(route);
+  if (route[1] === "friends") {
     res.statusCode = 200;
-    res.write("<html>");
-    res.write("<body>");
-    res.write("<p>Shinn Thant</p>");
-    res.write("<p>Thar Htet</p>");
-    res.write("<p>Kaung Kaung</p>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
+    res.setHeader("Content-Type", "application/json");
+
+    if (+route.length === 3) {
+      const friend_id = route[2];
+      const friend = friends.find((item) => item.id === +friend_id);
+      res.end(JSON.stringify(friend));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (route[1] === "writeHeader") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("Hello Im NodeJS Duddy");
   } else {
     res.statusCode = 404;
     res.end();
